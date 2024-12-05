b774014e2e5e22661ae2c64a1593ee78f7099b15
Jakub <jakub@schwenkbeck.de>
2024-12-05 16:39:45 +0100

    Fixed issues regarding positional_encoding and the unit tests

--- src/math/positional_encoding.rs
+++ src/math/positional_encoding.rs
@@ -1,8 +1,11 @@
 pub fn sinusoidal_pos_encoding(pos: usize, index: usize, embedding_size: usize) -> f32 {
-    if pos == 1 {return 0.0};
+    if pos == 0 {
+        return 0.0;
+    };
     let divisor = 10000f32.powf(2.0 * (index as f32 / embedding_size as f32)); // 100000^(2*i / embedding size)
 
-    if index % 2 == 0 { // for an even index, use sin, else cos!
+    if index % 2 == 0 {
+        // for an even index, use sin, else cos!
         (pos as f32 / divisor).sin()
     } else {
         (pos as f32 / divisor).cos()
