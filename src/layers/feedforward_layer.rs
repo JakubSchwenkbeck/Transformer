92f6911427684d2df8f417cd3f9a4466be4f39f0
Jakub <jakub@schwenkbeck.de>
2024-12-10 14:08:43 +0100

    Position-wise Feed-Forward function based on Chap3.3

--- src/layers/feedforward_layer.rs
+++ src/layers/feedforward_layer.rs
@@ -1,5 +1,9 @@
 #![allow(dead_code)]
+
+use crate::activation::activation_functions::gelu;
 use ndarray::Array2;
+use std::ops::Add;
+
 pub struct FeedForwardLayer {
     weights1: Array2<f32>,
     bias1: Array2<f32>, // weights and biases for first linear layer
@@ -29,5 +33,13 @@ impl FeedForwardLayer {
             bias2,
             dropout_rate: dropout,
         }
+    }
+
+    pub fn forward_t(&self, input: &Array2<f32>) -> Array2<f32> {
+        let first_dot = input.dot(&self.weights1);
+        let first_output = first_dot.add(&self.bias1);
+        let first_activation = gelu(&first_output);
+
+        gelu(&first_activation.dot(&self.weights2).add(&self.bias2))
     }
 }
